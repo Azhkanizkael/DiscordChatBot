942dbbb0bec58745767a845d2f7f27145a3ed037
Darrin Olsen <dolsen@cmnhospitals.org>
2020-10-28 23:54:40 -0500

    added ready

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const config = require("./config")
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-//Settings Database
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-//Bot Commands
 bot.login(config.discord.token);
